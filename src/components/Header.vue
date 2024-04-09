74213db54ea3c7c312c90cb522492cf6c8994c31
Simone Willemse <simywillemse@gmail.com>
2024-04-09 18:08:51 +0200

    played with light and dark mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,3 @@
-<script>
-// const activePage = window.location.pathname;
-// const navLinks = document.querySelectorAll("nav a").forEach((link) => {
-//   if (link.href.includes(`${activePage}`)) {
-//     link.classList.add("active");
-//   }
-// });
-// const pathName = window.location.pathname;
-// const pageName = pathName.split("/").pop();
-
-// if (pathName === "/") {
-//   document.querySelector(".work").classList.add("activeLink");
-// }
-// alert(pageName)
-//background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #212121;
-</script>
-
 <template>
   <nav>
     <div class="header">
@@ -53,29 +36,35 @@ nav {
   width: 100%;
   gap: 2rem;
   z-index: 5;
+  
 }
 
 .logo {
+  padding-left: 1rem;
   top: 0;
   left: 0;
   width: 50px;
   margin-top: 10px;
   z-index: 2;
 }
+
+.links{
+  padding-right: 1rem;
+}
+
 .menu {
   justify-content: flex-end;
   display: flex;
   flex-wrap: nowrap;
-  border: 2px solid rgba(225, 225, 225, 0.05);
+  border: 2px solid rgba(14, 13, 13, 0.153);
   padding: 5px;
   border-radius: 40px;
   z-index: 1;
   top: 0;
   opacity: 1;
-  backdrop-filter: blur(30px);
-  -webkit-backdrop-filter: blur(30px);
-
   background: rgba(225, 225, 225, 0.05);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
 
   inset: -1rem 0;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="hero">Hi there!</div>
+  <section class="container">
+    <div class="hero-text">HI, </div>
+    <div class="hero-text">I'M SIMONE WILLEMSE </div>
+    <div class="hero-sub-text">
+      A Graphic Designer & Front-end Developer
+    </div><br/>That likes creating stuff
+  </section>
 </template>
 
 <style>
-.hero {
-  margin-top: 60px;
-  width: 80vw;
-  height: 80vh;
-  border: 2px solid rgba(225, 225, 225, 0.05);
-  opacity: 1;
-  backdrop-filter: blur(30px);
-  -webkit-backdrop-filter: blur(30px);
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
 
-  background: rgba(225, 225, 225, 0.5);
-  padding: 5px;
-  border-radius: 40px;
+section {
+  display: grid;
+  place-items: left;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  margin-top: 150px;
+  padding: 100px;
+  border: 3px solid  rgba(14, 13, 13, 0.153);
+  background: rgba(225, 225, 225, 0.05);
+  border-radius: 50px;
+  backdrop-filter: blur(3px);
+ 
+}
+
+.hero-text {
+  font-size: 50px;
+  font-weight: 700;
+}
+.hero-sub-text {
+  display: flex;
+
+  font-size: 20px;
+  
 }
 </style>
